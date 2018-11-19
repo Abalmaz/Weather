ce841d3baa9da6d564cfbd803ac3e9420099b03e
Abalmaz <yana.abalmaz@gmail.com>
2018-11-19 14:08:37 +0200

    Created models for source and temperature data

--- my_weather/models.py
+++ my_weather/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Source(models.Model):
+    name = models.CharField(max_length=25)
+    is_update = models.BooleanField()
+    status = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Weather(models.Model):
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    date = models.DateField()
+    temperature = models.IntegerField()
