b35674acb68906c679857875c6f64f79f57d9bc0
Abalmaz <yana.abalmaz@gmail.com>
2018-11-19 15:47:39 +0200

    Show weather just for last 5 days

--- my_weather/views.py
+++ my_weather/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+from datetime import timedelta
 from django.views.generic.list import ListView
 
 from .models import Weather
@@ -8,3 +10,5 @@ class WeatherListView(ListView):
     model = Weather
     template_name = 'my_weather/show_weather.html'
     context_object_name = 'temperatures'
+    today = timezone.now() - timedelta(days=5)
+    queryset = Weather.objects.filter(date__gt=today).order_by('-date')
