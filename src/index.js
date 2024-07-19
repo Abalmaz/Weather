c8de56769389cbf14bc58ee01d6f99b2bf9ef921
Yana Abalmaz <yana.abalmaz@gmail.com>
2024-07-19 19:08:20 -0400

    Added SheCodes weather project

--- src/index.js
+++ src/index.js
@@ -1,63 +1,53 @@
-function getCurrentDateTime() {
-  let now = new Date();
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let weekDays = [
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
     "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday",
+    "Saturday"
   ];
-  let weekDay = weekDays[now.getDay()];
-  let month = months[now.getMonth()];
-  let date = now.getDate();
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  return `Today, ${month} ${date} from ${hours}:${minutes} <br> ${weekDay} `;
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
 }
 
-function celsiusToFahrenheit(celsius) {
-  return Math.round(celsius * (9 / 5) + 32);
-}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
 
-function fahrenheitToCelsius(fahrenheit) {
-  return Math.round(((fahrenheit - 32) * 5) / 9);
-}
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
 
-let todayDate = document.querySelector("#today-date");
-todayDate.innerHTML = getCurrentDateTime();
-
-function updateUnitTemperature(event) {
-  event.preventDefault();
-  let currentUnit = document.querySelector(".active");
-  let temperature = document.querySelector("#current-temperature");
-  if (event.target.innerHTML === "°F") {
-    let newTemperature = celsiusToFahrenheit(temperature.innerHTML);
-    temperature.innerHTML = newTemperature;
-    currentUnit.innerHTML = "°F";
-    event.target.innerHTML = "°C";
-  } else {
-    let newTemperature = fahrenheitToCelsius(temperature.innerHTML);
-    temperature.innerHTML = newTemperature;
-    currentUnit.innerHTML = "°C";
-    event.target.innerHTML = "°F";
-  }
-}
-
-let updateUnit = document.querySelector("a.inactive");
-updateUnit.addEventListener("click", updateUnitTemperature);
+currentDateELement.innerHTML = formatDate(currentDate);
